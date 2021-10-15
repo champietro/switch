f7135e7dacf02c554c3293490a8b87b0a1130694
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-14 21:47:41 -0300

    ert

--- index.ts
+++ index.ts
@@ -4,14 +4,9 @@ let sueldo = document.getElementById("sueldo");
 btnEnv.addEventListener("click", () => {
   let sueldoActual: number = sueldo.value;
   let sueldoFinal: number = 0;
-
   if (0 < sueldoActual && sueldoActual <= 15000) {
     sueldoFinal = (sueldoActual * 20) / 100;
-    console.log(
-      "%c Aumento de un 20%. Cobrará: $ ",
-      "background: #222; color: #bada55",
-      sueldoFinal
-    );
+    console.log("Aumento de un 20%. Cobrará: $ " + sueldoFinal);
   } else if (15001 < sueldoActual && sueldoActual <= 20000) {
     sueldoFinal = (sueldoActual * 10) / 100;
     console.log("Aumento de un 10%. Cobrará: $", sueldoFinal);
